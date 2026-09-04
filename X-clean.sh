5c7bd44a51b35f043ee4ed1b2555f84cb6b0de80
Ivan Mironov <mironov.ivan@gmail.com>
2026-09-05 00:13:47 +0200

    Ask for confirmation before removing the source trees

--- X-clean.sh
+++ X-clean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+printf "Write uppercase YES to remove OnStepX, SmartHandController and SmartWebServer source trees: "
+read confirm
+if [ "${confirm}" == "YES" ]; then
+    echo "Removing..."
+else
+    echo "Aborted!"
+    exit 1
+fi
+
 set -x -e -u -o pipefail
 
 [ -e "./X-clean.sh" ] || exit 1
